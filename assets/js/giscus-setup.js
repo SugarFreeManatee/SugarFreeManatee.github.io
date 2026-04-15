a347bab032843afd1c79b5ada7abd1d8c192b7e3
SugarFreeManatee <SugarFreeManatee@users.noreply.github.com>
2026-04-15 18:28:58 +0000

    Deploying to gh-pages from @ SugarFreeManatee/SugarFreeManatee.github.io@9142dce85a4a16ad2345b68749cd80de992722b7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SugarFreeManatee/sugarfreemanatee.github.io",
+    "data-repo": "SugarFreeManatee/SugarFreeManatee.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
